d7a638077852c8a0e044314fb632e26878a576e2
abhinandanreddyj <abhinandanreddy90@gmail.com>
2026-01-20 14:40:24 +0530

    Add files via upload

---
+++ java/forEachLoop.java
@@ -0,0 +1,11 @@
+package com;
+
+public class forEachLoop {
+    public static void main(String args[]) {
+        String array[] = {"Ron", "Harry", "Hermoine"};
+        //enhanced for loop
+        for (String x:array) {
+            System.out.println(x);
+        }
+    }
+}
