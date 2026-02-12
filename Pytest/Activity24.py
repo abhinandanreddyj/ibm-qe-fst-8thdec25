686cae66c11cc55519d656fccd5df3740b8ecfec
abhinandanreddyj <abhinandanreddy90@gmail.com>
2026-02-12 09:49:52 +0530

    Add files via upload

---
+++ Pytest/Activity24.py
@@ -0,0 +1,11 @@
+import pytest 
+
+@pytest.fixture
+def wallet():
+    amount = 0
+    return amount
+@pytest.mark.parametrize("earned, spent, expected", [(30, 10, 20),(20, 2, 18)])
+def test_wallet(wallet, earned, spent, expected):
+    wallet += earned
+    wallet -= spent
+    assert wallet == expected
